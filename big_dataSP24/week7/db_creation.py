7ff8acae7e859426d67598091c8d8cd186068c35
shearere2 <shearere2@duq.edu>
2024-03-13 15:34:07 -0400

    Temp change

--- big_dataSP24/week7/db_creation.py
+++ big_dataSP24/week7/db_creation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlalchemy
 from week6 import grants_reader
+from week6 import npi_reader
 
 
 def db():
@@ -10,9 +11,13 @@ def db():
 
 # WE MUST HAVE SAME COLUMNS IN DATAFRAME AND DATABASE!
 def npi_csv_to_db(csv_path: str):
-    df = grants_reader.read_grants_year(2022)
-    df.to_sql('npi',db(), if_exists='append',index=False) # method = 'multi' # chunksize=1000
+    df = npi_reader.read()
+    df.to_sql('npi',db(), if_exists='append',index=False)
 
+def grant_csv_to_db(csv_path: str):
+    df = grants_reader.read_grants_year(2022)
+    df.to_sql('grant',db(), if_exists='append',index=False) # method = 'multi' # chunksize=1000    
 
 if __name__ == "__main__":
     npi_csv_to_db('')
+    grant_csv_to_db('')
