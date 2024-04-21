8194bc5eb3bffcef1a3769f7f3b962bb8149fdc2
shearere2 <shearere2@duq.edu>
2024-04-21 16:38:52 -0400

    Updated script to have the correct labels. Still has incorrect features.

--- scripts/train_wine_nn.py
+++ scripts/train_wine_nn.py
@@ -1,7 +1,11 @@
 from big_dataSP24.tensortango import mlp,layer,train_nn
+from big_dataSP24.week4 import read_wine_data
+import pandas as pd
 if __name__ == "__main__":
-    features = 0
-    labels = 0
+    df = read_wine_data.read()
+    features = pd.read_csv('data/dimensionality_reduced_wine_2.csv')
+    print()
+    labels = df['quality']
     neural_net = mlp.MLP([layer.Tanh(2,2),
                           layer.Tanh(2,2)])
     train_nn(neural_net,features,labels)
